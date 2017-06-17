630b35a043ff258379fd46d16123d9760b33a11c
dcarrsf <dan@dancarr.co>
2017-06-17 11:30:45 -0700

    Updating App layout.

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,31 +4,25 @@ import './App.scss';
 class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       name: ''
     };
-
-    this.handleChange  = (e) => {
-      var newName = e.target.value;
-
-      this.setState({
-        name: newName
-      });
-    }
+    this.handleChange = this.handleChange.bind(this);
   }
-
+  handleChange(e) {
+    const newName = e.target.value;
+    this.setState({
+      name: newName
+    });
+  }
   render() {
     return (
       <div>
-        <h1>Hello World!!</h1>
-        <p>
-          Please input your name here:
-          <input ref="input"
+        <h1>Hello World!</h1>
+        <p>Please input your name here:</p>
+        <input ref="input"
             onChange={this.handleChange}
-            value={this.state.name}
-          />
-        </p>
+            value={this.state.name} />
         <p>Hello, <span ref="name">{this.state.name}</span> </p>
       </div>
     );
